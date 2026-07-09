44f881c5cc1254e2097bde0fda30b3a4f378a212
tharun-extinct <tharunkumar7041@gmail.com>
2026-07-09 10:42:03 +0530

    updated cargo.toml

---
+++ hello-macro/src/lib.rs
@@ -0,0 +1,11 @@
+/*  Marco Polo Game
+*/
+
+pub fn macro_polo(name: &str) -> String {
+    if name = "Macro" {
+        "Polo".to_string()
+    }
+    else {
+        "What's your name?".to_string();
+    }
+}
